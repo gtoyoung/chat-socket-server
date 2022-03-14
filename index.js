46640834477a149ddb996061cb4afc1c1f81d421
dovb <gtoyoung@naver.com>
2022-03-14 09:57:48 +0900

    소켓 서버 테스트

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const socketIo = require("socket.io")(server, {
   cors: {
     origin: process.env.TARGET_URL,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["*"],
     credentials: true,
   },
 });
 
 const socket = require("./src/socket");
 
-app.use(cors({ origin: process.env.TARGET_URL, credentials: true }));
-
-socket(socketIo);
+// app.use(cors({ origin: process.env.TARGET_URL, credentials: true }));
 
 app.get("/", function (req, res) {
   res.send("WELL COME! DOVB`s SOCKET SERVER");
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`server is running on port ${port}`);
+
+  socket(socketIo);
 });
