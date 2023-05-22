1c0e1e321b5e249dbccb42122dcd596f202e0ae6
ban <gtoyoung@naver.com>
2023-05-22 13:44:02 +0900

    코드 수정

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const cors = require("cors");
-// require("dotenv").config();
-// const corsOptions = {
-//   origin: "*",
-// };
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
 
-// 중요: 웹소켓을 위한 설정이라고 명시를 해주어야한다.
-const socketIo = require("socket.io")(server, {
+const app = express();
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer, {
   cors: {
     origin: process.env.TARGET_URL,
     methods: ["GET", "POST"],
@@ -20,9 +18,114 @@ const socketIo = require("socket.io")(server, {
 
 app.use(cors({}));
 
-const socket = require("./src/socket");
+const SOCKET_EVENT = {
+  JOIN_ROOM: "JOIN_ROOM",
+  SEND_MESSAGE: "SEND_MESSAGE",
+  RECEIVE_MESSAGE: "RECEIVE_MESSAGE",
+  LEAVE_ROOM: "LEAVE_ROOM",
+  TYPING_MESSAGE: "TYPING_MESSAGE",
+};
 
-socket(socketIo);
+// 방에 점속한 인원 수
+function countUsers(roomId) {
+  return wsServer.sockets.adapter.rooms.get(roomId)?.size;
+}
+
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket event: ${event}`);
+  });
+
+  // 방 참가
+  socket.on(SOCKET_EVENT.JOIN_ROOM, (requestData) => {
+    var type = SOCKET_EVENT.JOIN_ROOM;
+    socket.join(requestData.roomId);
+
+    // 현재 접속한 사용자 수
+    var connectedClients = countUsers(requestData.roomId);
+
+    const responseData = {
+      ...requestData,
+      type,
+      time: new Date(),
+      connectedClients,
+    };
+
+    wsServer.sockets.to(requestData.roomId).emit(SOCKET_EVENT.RECEIVE_MESSAGE, responseData);
+  });
+
+  // 방 퇴장
+  socket.on(SOCKET_EVENT.LEAVE_ROOM, (requestData) => {
+    var type = SOCKET_EVENT.LEAVE_ROOM;
+    socket.leave(requestData.roomId);
+
+    // 현재 접속한 사용자 수
+    var connectedClients = countUsers(requestData.roomId);
+
+    const responseData = {
+      ...requestData,
+      type,
+      time: new Date(),
+      connectedClients,
+    };
+
+    wsServer.sockets.to(requestData.roomId).emit(type, responseData);
+  });
+
+  // 메시지 타이핑 중
+  socket.on(SOCKET_EVENT.TYPING_MESSAGE, (requestData) => {
+    var type = SOCKET_EVENT.TYPING_MESSAGE;
+
+    // 현재 접속한 사용자 수
+    var connectedClients = countUsers(requestData.roomId);
+
+    const responseData = {
+      ...requestData,
+      type,
+      time: new Date(),
+      connectedClients,
+    };
+
+    wsServer.sockets.to(requestData.roomId).emit(type, responseData);
+  });
+
+  // 메시지 수신
+  socket.on(SOCKET_EVENT.RECEIVE_MESSAGE, (requestData) => {
+    var type = SOCKET_EVENT.RECEIVE_MESSAGE;
+
+    // 현재 접속한 사용자 수
+    var connectedClients = countUsers(requestData.roomId);
+
+    const responseData = {
+      ...requestData,
+      type,
+      time: new Date(),
+      connectedClients,
+    };
+
+    wsServer.sockets.to(requestData.roomId).emit(type, responseData);
+  });
+
+  socket.on(SOCKET_EVENT.SEND_MESSAGE, (requestData) => {
+    var type = SOCKET_EVENT.SEND_MESSAGE;
+
+    // 현재 접속한 사용자 수
+    var connectedClients = countUsers(requestData.roomId);
+
+    const responseData = {
+      ...requestData,
+      type,
+      time: new Date(),
+      connectedClients,
+    };
+
+    wsServer.sockets.to(requestData.roomId).emit(SOCKET_EVENT.RECEIVE_MESSAGE, responseData);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`"disconnect": ${reason}`);
+  });
+});
 
 app.get("/", function (req, res) {
   res.send("WELL COME! DOVB`s SOCKET SERVER");
@@ -30,6 +133,6 @@ app.get("/", function (req, res) {
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
